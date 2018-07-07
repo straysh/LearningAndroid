d033138a09edc0afc68a840aba0c7d7838218a09
straysh <jobhancao@gmail.com>
2018-07-07 21:28:09 +0800

    add CheatActivity and add it to manifest.xml

---
+++ app/src/main/java/com/straysh/androidprogramming/CheatActivity.java
@@ -0,0 +1,13 @@
+package com.straysh.androidprogramming;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class CheatActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+    }
+}
